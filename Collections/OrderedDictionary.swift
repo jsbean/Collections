8254cfd8e5e4c9881cb26b9245a8a3e595379e9f
jsbean <jbean46@gmail.com>
2016-12-10 02:20:29 -0500

    Migrate tests, cleanup

--- Collections/OrderedDictionary.swift
+++ Collections/OrderedDictionary.swift
@@ -22,6 +22,8 @@ public struct OrderedDictionary <K: Hashable, V> {
     /// Create an empty `OrderedDictionary`
     public init() { }
     
+    // MARK: - Subscripts
+    
     /// - returns: Value for the given `key`, if available. Otherwise, `nil`.
     public subscript(key: K) -> V? {
         
@@ -38,9 +40,13 @@ public struct OrderedDictionary <K: Hashable, V> {
             }
             
             let oldValue = values.updateValue(newValue, forKey: key)
-            if oldValue == nil { keys.append(key) }
+            if oldValue == nil {
+                keys.append(key)
+            }
         }
     }
+    
+    // MARK: - Instance Methods
     
     /// Append `value` for given `key`.
     public mutating func append(_ value: V, key: K) {
@@ -73,6 +79,53 @@ public struct OrderedDictionary <K: Hashable, V> {
     }
 }
 
+extension OrderedDictionary: Collection {
+    
+    // MARK: - `Collection`
+    
+    /// - Index after given index `i`.
+    public func index(after i: Int) -> Int {
+        
+        guard i != endIndex else {
+            fatalError("Cannot increment endIndex")
+        }
+        
+        return i + 1
+    }
+    
+    /// - Start index.
+    public var startIndex: Int {
+        return 0
+    }
+    
+    /// - End index.
+    public var endIndex: Int {
+        return keys.count
+    }
+    
+    /// - returns: Element at the given `index`.
+    public subscript (index: Int) -> (K,V) {
+        let key = keys[index]
+        let value = values[key]!
+        return (key, value)
+    }
+}
+
+extension OrderedDictionary: ExpressibleByDictionaryLiteral {
+    
+    // MARK: - `OrderedDictionary`
+    
+    /// Create an `OrderedDictionary` with a `DictionaryLiteral`.
+    public init(dictionaryLiteral elements: (K,V)...) {
+        
+        self.init()
+        
+        elements.forEach { (k, v) in
+            append(v, key: k)
+        }
+    }
+}
+
 /// - returns: `true` if all values contained in both `OrderedDictionary` values are
 /// equivalent. Otherwise, `false`.
 public func == <K, V: Equatable> (lhs: OrderedDictionary<K,V>, rhs: OrderedDictionary<K,V>)
@@ -98,19 +151,4 @@ public func == <K, V: Equatable> (lhs: OrderedDictionary<K,V>, rhs: OrderedDicti
     }
     
     return true
-}
-
-extension OrderedDictionary: ExpressibleByDictionaryLiteral {
-    
-    // MARK: - `OrderedDictionary`
-    
-    /// Create an `OrderedDictionary` with a `DictionaryLiteral`.
-    public init(dictionaryLiteral elements: (K,V)...) {
-        
-        self.init()
-        
-        elements.forEach { (k, v) in
-            append(v, key: k)
-        }
-    }
 }
