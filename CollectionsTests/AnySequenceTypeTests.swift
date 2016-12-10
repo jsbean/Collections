5a36de960e5382198fd4dc6606b5629765d286af
jsbean <jbean46@gmail.com>
2016-12-09 23:43:51 -0500

    Migrate AnySequenceType

---
+++ CollectionsTests/AnySequenceTypeTests.swift
@@ -0,0 +1,37 @@
+//
+//  AnySequenceTypeTests.swift
+//  Collections
+//
+//  Created by James Bean on 12/9/16.
+//
+//
+
+import XCTest
+import Collections
+
+class AnySequenceTypeTests: XCTestCase {
+    
+    struct TestSequence: AnySequenceType {
+        
+        typealias Element = Int
+        
+        var sequence: AnySequence<Int> {
+            return AnySequence(array)
+        }
+        
+        let array: [Int]
+        
+        init <S: Sequence> (_ sequence: S) where S.Iterator.Element == Element {
+            self.array = Array(sequence)
+        }
+        
+        init(arrayLiteral elements: Int...) {
+            self.array = Array(elements)
+        }
+    }
+    
+    func testIterator() {
+        let ts = TestSequence([1,2,3])
+        XCTAssertEqual(ts.map { $0 }, [1,2,3])
+    }
+}
