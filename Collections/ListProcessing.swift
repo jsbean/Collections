fd242b63ed27a8188d9dffff4ae42ed12ab090b6
jsbean <jbean46@gmail.com>
2016-12-23 15:24:09 +0100

    Tidy up doc comments, add breathing room

--- Collections/ListProcessing.swift
+++ Collections/ListProcessing.swift
@@ -25,11 +25,8 @@ extension Array {
         return Array(self[1..<self.endIndex])
     }
     
-    /**
-     2-tuple containing the `head` `Element` and `tail` `[Element]` of `Self`
-     
-     -  note: From Chris Eidhof: http://chris.eidhof.nl/posts/swift-tricks.html
-     */
+    /// 2-tuple containing the `head` `Element` and `tail` `[Element]` of `Self`
+    /// - author: Chris Eidhof (see: http://chris.eidhof.nl/posts/swift-tricks.html)
     public var destructured: (Element, Array<Element>)? {
         guard let head = head, let tail = tail else { return nil }
         return (head, tail)
@@ -49,27 +46,20 @@ extension ArraySlice {
         return self[(self.startIndex + 1) ..< self.endIndex]
     }
     
-    /**
-     2-tuple containing the `head` and `tail`.
-     
-     -  note: From Chris Eidhof: http://chris.eidhof.nl/posts/swift-tricks.html
-     */
+    /// 2-tuple containing the `head` `Element` and `tail` `ArraySlice<Element>` of `Self`
+    /// - author: Chris Eidhof (see: http://chris.eidhof.nl/posts/swift-tricks.html)
     public var destructured: (Element, ArraySlice<Element>)? {
         guard let head = head, let tail = tail else { return nil }
         return (head, tail)
     }
 }
 
-/**
- - returns: New `Array` with the first element `head`, and the remaining elements of `tail`.
- */
-public func + <T>(head: T, tail: [T]) -> [T] {
+/// - returns: New `Array` with the first element `head`, and the remaining elements of `tail`.
+public func + <T> (head: T, tail: [T]) -> [T] {
     return [head] + tail
 }
 
-/**
- - returns: New `Array` with `item` appended to the end of `list`.
- */
-public func + <T>(list: [T], item: T) -> [T] {
+/// - returns: New `Array` with `item` appended to the end of `list`.
+public func + <T> (list: [T], item: T) -> [T] {
     return list + [item]
 }
