042a2ed15fd9f036ebb5e712ec5da137f4360e82
James <jbean46@gmail.com>
2017-06-07 13:57:10 +0200

    Add CircularArray.removing(at:)

--- Collections/CircularArray.swift
+++ Collections/CircularArray.swift
@@ -56,6 +56,11 @@ public struct CircularArray<Element> {
         return self[from: start + 1, through: end - 1]
     }
     
+    /// - Returns: A new `CircularArray` with the element removed at the given `index`.
+    public func removing(at index: Int) -> CircularArray<Element> {
+        return CircularArray(self.storage.removing(at: index))
+    }
+    
     private func circularIndex(_ index: Int) -> Int {
         return mod(index, storage.count)
     }
