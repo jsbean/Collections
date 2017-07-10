898153ae040863cd00378ba11b622c00cca3d830
Brian Heim <brianlheim@gmail.com>
2017-07-10 16:56:22 -0400

    Add new testing frameworks for performance testing

---
+++ Collections macOS Performance Tests/Collections_macOS_Performance_Tests.swift
@@ -0,0 +1,20 @@
+//
+//  Collections_macOS_Performance_Tests.swift
+//  Collections macOS Performance Tests
+//
+//  Created by Brian Heim on 7/10/17.
+//
+//
+
+import XCTest
+import Collections
+
+class SortedArrayPerformanceTests : XCTestCase {
+
+    func testPerformanceExample() {
+        self.measure {
+            // TODO: fill out tests
+        }
+    }
+
+}
