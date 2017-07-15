ae2f5189c144e153fccd9dbc2d4ac9ff319341ac
James <jbean46@gmail.com>
2017-07-15 01:06:36 +0100

    Create SortedCollectionWrapping

---
+++ Collections/SortedCollectionWrapping.swift
@@ -0,0 +1,23 @@
+//
+//  SortedCollectionWrapping.swift
+//  Collections
+//
+//  Created by James Bean on 7/15/17.
+//
+//
+
+public protocol SortedCollectionWrapping {
+    associatedtype Base: BidirectionalCollection
+    var base: Base { get }
+}
+
+extension SortedCollectionWrapping {
+
+    public func min() -> Base.Iterator.Element? {
+        return base.first
+    }
+
+    public func max() -> Base.Iterator.Element? {
+        return base.last
+    }
+}
