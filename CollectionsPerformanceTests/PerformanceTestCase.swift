61578debea40c84a32c606d7c6b0309a5a2752c9
Brian Heim <brianlheim@gmail.com>
2017-08-05 12:48:38 -0400

    perftest: add linear regression & assertion methods

--- CollectionsPerformanceTests/PerformanceTestCase.swift
+++ CollectionsPerformanceTests/PerformanceTestCase.swift
@@ -171,6 +171,92 @@ func testMutatingOperation<C>(
     }
 }
 
+func assertLinearPerformance(_ data: [(Double, Double)]) {
+    let (slope, intercept, rvalue) = linearRegression(data)
+    let expectedSlope: Double = 1
+    let expectedSlopeAccuracy = 0.01
+    let expectedRValueMin = 0.99
+
+    // debug
+    print("slope: \(slope), expected \(expectedSlope) with accuracy \(expectedSlopeAccuracy)")
+    print("intercept: \(intercept)")
+    print("rvalue: \(rvalue), expected min \(expectedRValueMin)")
+
+    XCTAssertEqual(slope, expectedSlope, accuracy: expectedSlopeAccuracy)
+    XCTAssert(rvalue < expectedRValueMax)
+}
+
+func assertConstantPerformance(_ data: [(Double, Double)]) {
+    let (slope, intercept, rvalue) = linearRegression(data)
+    let expectedSlope: Double = 0
+    let expectedSlopeAccuracy = 0.01
+    let expectedRValueMin = 0.99
+
+    // debug
+    print("slope: \(slope), expected \(expectedSlope) with accuracy \(expectedSlopeAccuracy)")
+    print("intercept: \(intercept)")
+    print("rvalue: \(rvalue), expected min \(expectedRValueMax)")
+
+    XCTAssertEqual(slope, expectedSlope, accuracy: expectedSlopeAccuracy)
+    XCTAssert(rvalue < expectedRValueMax)
+}
+
+func assertLogPerformance(_ data: [(Double, Double)]) {
+    let logData = data.map { ($0, exp($1)) }
+    let (slope, intercept, rvalue) = linearRegression(logData)
+    let expectedSlope: Double = 0
+    let expectedSlopeAccuracy = 0.01
+    let expectedRValueMax = 0.01
+
+    // debug
+    print("slope: \(slope), expected \(expectedSlope) with accuracy \(expectedSlopeAccuracy)")
+    print("intercept: \(intercept)")
+    print("rvalue: \(rvalue), expected max \(expectedRValueMax)")
+
+    XCTAssertEqual(slope, expectedSlope, accuracy: expectedSlopeAccuracy)
+    XCTAssert(rvalue < expectedRValueMax)
+}
+
+func linearRegression(_ data: [(Double, Double)]) -> (Double, Double, Double) {
+    let xs = data.map { $0.0 }
+    let ys = data.map { $0.1 }
+    let sumOfXs = xs.reduce(0, +)
+    let sumOfYs = ys.reduce(0, +)
+    let sumOfXsSquared = xs.map { pow($0, 2) }.reduce(0, +)
+    let sumOfXsTimesYs = data.map(*).reduce(0, +)
+
+    let denominator = Double(data.count) * sumOfXsSquared - pow(sumOfXs, 2)
+    let interceptNumerator = sumOfYs * sumOfXsSquared - sumOfXs * sumOfXsTimesYs
+    let slopeNumerator = Double(data.count) * sumOfXsTimesYs - sumOfXs * sumOfYs
+
+    let intercept = interceptNumerator / denominator
+    let slope = slopeNumerator / denominator
+    let regressionCoefficient = calculateRegressionCoefficient(data, sumOfXs: sumOfXs, sumOfYs: sumOfYs, slope: slope)
+
+    return (slope, intercept, regressionCoefficient)
+}
+
+func calculateRegressionCoefficient(_ data: [(Double, Double)], sumOfXs: Double, sumOfYs: Double, slope: Double) -> Double {
+
+    let meanOfYs = sumOfYs / Double(data.count)
+    let squaredErrorOfYs = data.map { pow($0.1 - meanOfYs, 2) }.reduce(0, +)
+    let denominator = squaredErrorOfYs
+
+    // debugging
+    print("\(#function): denominator: \(denominator)")
+
+    guard denominator != 0 else { return 0 }
+
+    let meanOfXs = sumOfXs / Double(data.count)
+    let squaredErrorOfXs = data.map { pow($0.0 - meanOfXs, 2) }.reduce(0, +)
+    let numerator = squaredErrorOfXs
+
+    // debugging
+    print("\(#function): numerator: \(numerator)")
+
+    return sqrt(numerator / denominator) * slope
+}
+
 class CollectionPerformanceTestCase: XCTestCase {
 
     func testRefactor() {
